5b8f1ff07bf8212731be844eaf8959e77800b491
dorcax <dorcasibrahim667@gmail.com>
2024-03-19 02:54:27 +0100

    fixed

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -15,18 +15,18 @@ export default {
   ],
   theme: {
     extend: {
-      // screens: {
+      screens: {
       //   // sm: '640px',
       //   // // => @media (min-width: 640px) { ... }
   
   
      
-      //   'sm': {'max-width': '649px'},  // up to 639px
-      //   'md': {'max-width': '767px'},  // up to 767px
-      //   'lg': {'max-width': '1023px'}, // up to 1023px
-      //   'xl': {'max-width': '1279px'}, // up to 1279px
-      //   '2xl': {'max-width': '1535px'},// up to 1535px
-      // },
+        'sm': {'max-width': '649px'},  // up to 639px
+        'md': {'max-width': '767px'},  // up to 767px
+        'lg': {'max-width': '1023px'}, // up to 1023px
+        'xl': {'max-width': '1279px'}, // up to 1279px
+        '2xl': {'max-width': '1535px'},// up to 1535px
+      },
     },
   },
   plugins: [],
